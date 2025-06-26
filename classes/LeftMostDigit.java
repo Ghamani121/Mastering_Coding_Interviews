180e0e7c10cf901e67dde564d684c676c3c05fe5
ghamani <138302691+Ghamani121@users.noreply.github.com>
2025-06-26 08:47:25 +0530

    Update LeftMostDigit.java

--- classes/LeftMostDigit.java
+++ classes/LeftMostDigit.java
@@ -2,3 +2,15 @@
 // Define a function to accept a number as parameter and return its leftmost digit.
 // Eg. 1: input=285, output=2
 // Eg. 2: input=76429, output=7
+class LeftMostDigit 
+{
+ public int Count(int n)
+ {
+   while(n>9) n/=10;   
+   return n;
+  }
+  public static void main(String[] args) {
+    LeftMostDigit obj = new LeftMostDigit();
+    System.out.println(obj.Count(285));
+  }
+}
