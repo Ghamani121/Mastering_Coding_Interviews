b219ef2775ac53940db4139eb428dfe73933f3a5
ghamani <138302691+Ghamani121@users.noreply.github.com>
2025-07-26 15:49:19 +0530

    leaning somedart

---
+++ random/conditions.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  String destinationZone='PQR';
+  double weightInKg=10.0;
+  if(destinationZone=='XYZ') 
+  print("shipping cost is \$50");
+  else if(destinationZone=='PQR')
+  print("shipping cost is \$60");
+  else if(destinationZone=='ABC')
+  print("shipping cost is \$70");
+  else 
+  print("enter the correct zone");
+}
